3aa0b16e62c8c8acd38003cc220690338612a14f
grm34 <fripouillejack@gmail.com>
2014-12-02 01:40:02 +0100

    Add HB duration value

--- app/mod_encode/scan_source.py
+++ app/mod_encode/scan_source.py
@@ -19,20 +19,22 @@ def scan(source):
         # Join mediainfo output
         mediainfo = ''.join(scan_MI)
 
-        # Splitlines HB output and get autocrop lines
+        # Splitlines HB output and get autocrop & duration
         HB_data = scan_HB.splitlines()
         [ar_infos, autocrop] = ['', ] * 2
 
         for line in HB_data:
             if 'autocrop' in line and 'aspect' in line:
                 ar_infos = line
-            elif '+ autocrop' in line:
+            if '+ autocrop' in line:
                 autocrop = line.replace('+', '')
+            if 'duration' in line:
+                duration = line.split(',')[-1]
 
         # Display HB & mediainfo parsed results
-        scan_data = '{0}\n\n{1}\n\n{2}'.format(ar_infos.strip(),
-                                               mediainfo.strip(),
-                                               autocrop.strip())
+        scan_data = '{0}\n{3}\n{1}\n\n{2}\n\n{1}\n{3}\n{0}'\
+                    .format(ar_infos.strip(), duration.strip(),
+                            mediainfo.strip(), autocrop.strip())
         return scan_data
 
     except OSError as e:

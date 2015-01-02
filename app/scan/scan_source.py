27eb82656afb88296c900851dd47a880f3e21604
grm34 <fripouillejack@gmail.com>
2015-01-02 12:39:12 +0100

    Implicit platform cmds

--- app/scan/scan_source.py
+++ app/scan/scan_source.py
@@ -1,5 +1,6 @@
 #!/usr/bin/kivy
 import sys
+import platform
 import subprocess
 
 
@@ -9,21 +10,38 @@ def scan(source):
     Return complete mediainfo and HandBrake autocrop '''
     try:
 
-        # Get HB & mediainfo outputs
-        scan_MI = subprocess.getoutput('mediainfo {}'.format(source))
-        scan_HB = subprocess.getoutput('HandBrakeCLI -t 0 --scan -i {}'
-                                       .format(source))
+        # Get user platform
+        user_os = platform.system()
 
-        # Join mediainfo output
-        mediainfo = ''.join(scan_MI)
+        # Windows users
+        if 'Windows' in user_os:
+            scan_MI = subprocess.check_output(
+                'contrib\mediainfo\MediaInfo.exe {}'.format(source))
+            mediainfo = '{}'.format(
+                scan_MI.decode('utf-8').replace('\r', ''))
+            scan_HB = subprocess.check_output(
+                'contrib\handbrake\HandBrakeCLI.exe'
+                ' -t 0 --scan -i {}'.format(source),
+                stderr=subprocess.STDOUT, shell=True)
+            handbrake = '{}'.format(
+                scan_HB.decode('utf-8').replace('\r', ''))
+            HB_data = handbrake.splitlines()
 
-        # Splitlines HB output and get autocrop & duration
-        HB_data = scan_HB.splitlines()
-        [ar_infos, autocrop] = ['', ] * 2
+        # Unix users
+        elif 'Linux' in user_os:
+            scan_MI = subprocess.getoutput(
+                'contrib/mediainfo/MediaInfo_unix {}'.format(source))
+            mediainfo = ''.join(scan_MI)
+            scan_HB = subprocess.getoutput(
+                'contrib/handbrake/HandBrakeCLI_unix -t 0'
+                ' --scan -i {}'.format(source))
+            HB_data = scan_HB.splitlines()
 
+        # Get autocrop & duration
+        [ar_infos, autocrop, duration] = ['', ] * 3
         for line in HB_data:
             if 'autocrop' in line and 'aspect' in line:
-                ar_infos = line
+                ar_infos = line.split('] ')[-1]
             if '+ autocrop' in line:
                 autocrop = line.replace('+', '')
             if 'duration' in line:
